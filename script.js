0054e3183cf923ae9fc1c459c3ac4997b832f0fc
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2020-04-07 14:58:36 -0300

    Added features using functions

--- script.js
+++ script.js
@@ -17,7 +17,56 @@ function showSuccess(input) {
   formControl.classList.add('success');
 }
 
-function isValidEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+function checkEmail(input) {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (regex.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email not valid');
+  }
 }
+
+//check required fields
+
+function checkRequired(inputArray) {
+  inputArray.forEach(input => {
+    if (input.value.trim() === '') {
+      showError(input, `${input.id} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+//check input lenght
+function checkLenght(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `Must have: ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `Must have maximum: ${max}`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+//check password confirmation
+function checkPassword(password1, password2) {
+  if (password1.value === password2.value) {
+    showSuccess([password1, password2]);
+  } else {
+    showError(password2, 'Password is not equal');
+  }
+}
+
+//Event Listener
+form.addEventListener('submit', e => {
+
+  e.preventDefault();
+  checkRequired([user, email, password, password2]);
+  checkLenght(user, 3, 12);
+  checkLenght(password, 6, 16);
+  checkEmail(email);
+  checkPassword(password, password2);
+
+});
